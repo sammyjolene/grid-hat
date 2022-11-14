a4505e285a37095274a059c5476771a45022794c
Sammy Jolene <sammyshorey@gmail.com>
2022-11-14 10:49:18 -0500

    Print letters row by row

---
+++ grid.py
@@ -0,0 +1,7 @@
+s_row = ['.xxxx.', 'xxxxxx', 'xxx...', '.xxxx.', '...xxx', '....xx', 'xxxxxx', '.xxxx.']
+a_row = ['.xxxx.', 'xxxxxx', 'xx..xx', 'xx..xx', 'xxxxxx', 'xxxxxx', 'xx..xx', 'xx..xx']
+m_row = ['x....x', 'xx..xx', 'xxxxxx', 'xxxxxx', 'xx..xx', 'xx..xx', 'xx..xx', 'xx..xx']
+
+print(*s_row, sep = "\n")
+print(*a_row, sep = "\n")
+print(*m_row, sep = "\n")
